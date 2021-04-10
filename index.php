0a6016e47256ff6ff63c679bf3b4a12727a1051e
tiagoornelas <advtiagoornelas@gmail.com>
2021-04-10 17:23:26 -0300

    Criação da página para acesso do consumidor no estilo Landing Page

--- index.php
+++ index.php
@@ -21,56 +21,60 @@
 		<div id="banner_de_cima">
 			<img width=100% src="img/banner_de_cima.jpg">
 		</div>
-
-									<!-- 2 - LISTA DE FORNECEDORES -->
-		<div id="listaFornecedores">
-			<ul class="margemDeLista">
-				<li class="inline"><img class="fornecedor"
-							src="img/lopas.jpg"></li>
-				<li class="inline"><img class="fornecedor"
-							src="img/valdemoveis.jpg"></li>
-				<li class="inline"><img class="fornecedor"
-							src="img/bianchi.png"></li>
-				<li class="inline"><img class="fornecedor"
-							src="img/salleto.jpg"></li>
-				<li class="inline"><img class="fornecedor"
-							src="img/kaiki.jpg"></li>
-				<li class="inline"><img class="fornecedor"
-							src="img/benetil.jpg"></li>
-				<li class="inline"><img class="fornecedor"
-							src="img/carioca.jpg"></li>
-				<li class="inline"><img class="fornecedor"
-							src="img/tcil.png"></li>
-			</ul>
-		</div>
 	 							<!-- 3 - BANNER PRINCIPAL -->
 								<!-- 3.1 - Ícone de Chave no Banner Principal -->
-			<div id="banner">
-				<img id="chave" src="img/tools.jpg" align="left">
-								<!-- 3.2 - Imagem da Direita do Banner Principal -->
-				<img id="pixelart_sfg" src="img/meomoveis_sfg.jpg" align="right">
-									<!-- 3.3 - Texto do Banner -->
-				<h1 class="tituloBanner">
-					único site da região que<br></h1>
-				<h2 id="subtituloBanner">
-					entrega e monta!</h2>
-			</div>
-									<!-- 4 - CATEGORIAS -->
-			<div>
-				<h1 id="tituloCategoria">o que você está precisando?</h1>
-				<ul id="listaCategorias">
-					<li class="inline"><img class="categorias"
-								src="img/cat_cozinha.jpg"></li>
-					<li class="inline"><img class="categorias"
-								src="img/cat_sala_de_estar.jpg"></li>
-					<li class="inline"><img class="categorias"
-								src="img/cat_quarto.jpg"></li>
-					<li class="inline"><img class="categorias"
-								src="img/cat_escritorio.jpg"></li>
-					<li class="inline"><img class="categorias"
-								src="img/cat_sala_de_jantar.jpg"></li>
-					<li class="inline"><img class="categorias"
-								src="img/cat_quarto_infantil.jpg"></li>
+		<div id="imageLandingPage">
+			<img width=100% src="img/landing.jpg">
+		</div>
+
+		<div id="landingPage">
+			<br>
+			<h1 id="tituloCategoria">Compre móveis com preço de fábrica e montagem grátis.</h1>
+			<h2 id="subtituloCategoria">Mais de 70 anos de experiência no ramo, agora na internet.
+			</h2>
+			<h2 id="textoCategoria">Somos a única loja online da região que oferece preços
+			direto da fábrica e serviço de montagem inclusos.<br>
+			Aproveite para ver as nossas ofertas da semana e se surpreenda.
+			</h2>
+			<button id="buttonLanding"><a class="HideLink" target="_blank" href="https://api.whatsapp.com/send?phone=553237541101&text=Ol%C3%A1%2C%20gostaria%20de%20ver%20as%20ofertas%20da%20semana!">
+			VER OFERTAS DA SEMANA</a></button>
+		</div>
+		<div id="banner2">
+			<img width=70% src="img/banner-2.jpg">
+		</div>
+		<div id="imageLandingPage2">
+			<img width=100% src="img/landing2.jpg">
+		</div>
+		<div id="landingPage">
+			<br>
+			<h1 id="tituloCategoria">Móveis para todos os tipos de casa.</h1>
+			<h2 id="subtituloCategoria">Nossos produtos atendem as demandas de todos os tipos de consumidores, seja para sua casa ou negócio.
+			</h2>
+			<h2 id="textoCategoria">Trabalhamos com mais de 80 fábricas e nosso catálogo tem mais de 1000 variações de produtos.<br><br>
+			A variedade de fábricas garante que teremos, todas as semanas, as melhores soluções e os melhores preços, aliando atendimento personalizado
+			e agilidade na entrega, para garantir que você tenha o que precisa, no preço que pode pagar.
+			</h2>
+			<button id="buttonLanding"><a class="HideLink" target="_blank" href="https://api.whatsapp.com/send?phone=553237541101&text=Ol%C3%A1%2C%20gostaria%20de%20conhecer%20os%20seus%20produtos!">
+				CONHECER NOSSOS PRODUTOS</a></button>
+		</div>
+			<div id="listaFornecedores">
+				<ul class="margemDeLista">
+					<li class="inline"><img class="fornecedor"
+								src="img/lopas.jpg"></li>
+					<li class="inline"><img class="fornecedor"
+								src="img/valdemoveis.jpg"></li>
+					<li class="inline"><img class="fornecedor"
+								src="img/bianchi.png"></li>
+					<li class="inline"><img class="fornecedor"
+								src="img/salleto.jpg"></li>
+					<li class="inline"><img class="fornecedor"
+								src="img/kaiki.jpg"></li>
+					<li class="inline"><img class="fornecedor"
+								src="img/benetil.jpg"></li>
+					<li class="inline"><img class="fornecedor"
+								src="img/carioca.jpg"></li>
+					<li class="inline"><img class="fornecedor"
+								src="img/tcil.png"></li>
 				</ul>
 			</div>
 	</main>
