7af8e91c3e1ba043c51b911c119b9c4647558666
tiagoornelas <advtiagoornelas@gmail.com>
2021-04-11 09:27:59 -0300

    Adição de cola no arquivo dbh-inc para troca rápida de banco de dados no ambiente local de testes

--- includes/dbh-inc.php
+++ includes/dbh-inc.php
@@ -10,3 +10,17 @@ $conn = mysqli_connect($serverName, $dBUsername, $dBPassword, $dBName);
 if (!$conn) {
   die("Conexão com o banco de dados falhou: " . mysqli_connect_error());
 }
+
+# COLA PARA SERVIDOR HOSTINGER
+#
+# $serverName = "localhost";
+# $dBUsername = "u573611555_tiago";
+# $dBPassword = "D6YT?juC*5zI";
+# $dBName = "u573611555_meomoveis";
+#
+# COLA PARA SERVIDOR LOCAL
+#
+# $serverName = "localhost";
+# $dBUsername = "tiago";
+# $dBPassword = "password";
+# $dBName = "meomoveis";
